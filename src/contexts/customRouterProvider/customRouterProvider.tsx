3b8f759d1a8c45dd6677f1f8ff5faecb0653c597
KaisAlHusrom <kais.al.husrom@gmail.com>
2024-11-03 17:33:03 +0300

    add gh-pages

--- src/contexts/customRouterProvider/customRouterProvider.tsx
+++ src/contexts/customRouterProvider/customRouterProvider.tsx
@@ -2,33 +2,36 @@ import {
     createBrowserRouter,
     createRoutesFromElements,
     Route,
-    RouterProvider
+    RouterProvider,
 } from 'react-router-dom';
 
 //pages
 import { ErrorPage, LandPage, LandPageRoot, NotFoundPage, Root } from '@/pages';
 
+const REPO_NAME = 'Kais.Husrom';
+
 //Router
 const router = createBrowserRouter(
     createRoutesFromElements(
         //All App Routes
-        <Route path='/' element={<Root />} errorElement={<ErrorPage />} >
-
-
+        <Route path='/' element={<Root />} errorElement={<ErrorPage />}>
             <Route path='' element={<LandPageRoot />}>
                 <Route path='' element={<LandPage />} />
             </Route>
 
-            <Route path="*" element={<NotFoundPage />} />
-        </Route>
-    )
-)
-
+            <Route path='*' element={<NotFoundPage />} />
+        </Route>,
+    ),
+    {
+        basename:
+            process.env.NODE_ENV === 'production'
+                ? `/${REPO_NAME}` // In production, use the repository name
+                : '/', // In development, use root path
+    },
+);
 
 const CustomRouterProvider = () => {
-    return (
-        <RouterProvider router={router} />
-    );
+    return <RouterProvider router={router} />;
 };
 
 export default CustomRouterProvider;
